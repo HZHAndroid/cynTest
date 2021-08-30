2c758c8fa18068b6ae1b9bece9d38f3b5d04691a
蓝月羽 <1104090872@qq.com>
2021-08-30 20:51:42 +0800

    first commit

---
+++ cyn/src/main/java/com/young/cyn/CynTest.kt
@@ -0,0 +1,30 @@
+package com.young.cyn
+
+/**
+ * @author:  Young
+ * .
+ * @email:   1104090872@qq.com
+ * .
+ * @date :   2021/8/30 20:44 周一
+ * 　　 へ　　　　　／|
+　　/＼7　　∠＿/
+　 /　│　　 ／　／
+　│　Z ＿,＜　／　　 /`ヽ
+　│　　　　　ヽ　　 /　　〉
+　 Y　　　　　`　 /　　/
+　ｲ●　､　●　　⊂⊃〈　　/
+　()　 へ　　　　|　＼〈
+　　>ｰ ､_　 ィ　 │ ／／
+　 / へ　　 /　ﾉ＜| ＼＼
+　 ヽ_ﾉ　　(_／　 │／／
+　　7　　　　　　　|／
+　　＞―r￣￣`ｰ―＿
+
+ * @description:
+ */
+class CynTest {
+
+    fun calculation(num: Int): Int {
+        return num * 2
+    }
+}
